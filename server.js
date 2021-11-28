de3d9ec3bd32eb31a3b7262e14c897aa97df748e
paramesh-p98 <paramesh3598@gmail.com>
2021-11-28 05:52:26 +0530

    removing node_env for heroku to setup its own

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ if (process.env.NODE_ENV === 'development') {
 
 app.use('/api/v1/transactions', transactions);
 
-if (process.env.NODE_ENV === 'PRODUCTION') {
+if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/public'));
 
     app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'public', 'index.html')));
